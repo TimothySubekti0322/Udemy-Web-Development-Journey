09303c497d2fb0eb9265069ab61d12a635f0bbba
TimothySubekti0322 <18221063@std.stei.itb.ac.id>
2023-07-14 21:53:07 +0700

    tidy up

--- Section 10 - 19/Section 19 - JQuery/index.js
+++ Section 10 - 19/Section 19 - JQuery/index.js
@@ -6,9 +6,8 @@
 // $(Element).text(<the text that you want>)
 
 // Fungsi Menambah Kelas "Cliked" pad Button jika di klik
-for(let i = 0 ; i < $(".first-button").length ; i++)
-{
-    $(".first-button").eq(i).click(function() {
+for (let i = 0; i < $(".first-button").length; i++) {
+    $(".first-button").eq(i).click(function () {
         // if($("Button")[i].hasClass("clicked")) {
         //     $("Button")[i].removeClass("clicked");
         //     $("Button")[i].text("Click Me");
@@ -18,14 +17,14 @@ for(let i = 0 ; i < $(".first-button").length ; i++)
         //     $("Button")[i].text("CLICKED");
         // }
         $(this).toggleClass("clicked")
-        
+
         var text = $(this).text();
         $(this).text(text === "Click Me" ? "CLICKED" : "Click Me");
     })
 }
 
 // Membuat Fungso untuk Mengubah tulisan Another hello -> Hello jika di Klik
-document.querySelectorAll("h1")[1].addEventListener("click", function() {
+document.querySelectorAll("h1")[1].addEventListener("click", function () {
     var text = document.querySelectorAll("h1")[1].textContent;
     document.querySelectorAll("h1")[1].textContent = (text === "Another Hello" ? "Hello" : "Another Hello");
 })
@@ -36,9 +35,8 @@ console.log($("img").attr("src")) //This will get attribute from img
 
 // Membuat Fungsi Mengganti H1 berdasarkan Masukan text di input
 var h1 = "";
-$("input").keypress(function(event) {
-    if(event.key === "Enter")
-    {
+$("input").keypress(function (event) {
+    if (event.key === "Enter") {
         $("h1.timothy.subekti").text(h1);
         h1 = "";
     }
@@ -48,7 +46,7 @@ $("input").keypress(function(event) {
 })
 
 // Mengubah Warna Tulisan "Hello" menjadi pruple jika di klik
-$("h1.timothy").on("click", function() {
+$("h1.timothy").on("click", function () {
     var color = ($(this).css("color") == 'rgb(0, 0, 0)' ? "purple" : "black");
     console.log(color);
     $("h1.timothy").css("color", color);
@@ -56,35 +54,35 @@ $("h1.timothy").on("click", function() {
 
 // Menambahkan new Button berdasarkan pilihan before - after - prepend - append
 
-$("#mySelect").on("change",function() {
+$("#mySelect").on("change", function () {
     var selected = $(this).find("option:selected");
-    if(selected.hasClass("option-before")) {
+    if (selected.hasClass("option-before")) {
         $(".before").before("<Button class='btn'> New Button </Button>");
     }
-    else if(selected.hasClass("option-after")) {
+    else if (selected.hasClass("option-after")) {
         $(".after").after("<Button class='btn'> New Button </Button>");
     }
-    else if(selected.hasClass("option-prepend")) {
+    else if (selected.hasClass("option-prepend")) {
         $(".prepend").prepend("<Button class='btn'> New Button </Button>");
     }
-    else if(selected.hasClass("option-append")) {
+    else if (selected.hasClass("option-append")) {
         $(".append").append("<Button class='btn'> New Button </Button>");
     }
-    else if(selected.hasClass("refreshed")) {
+    else if (selected.hasClass("refreshed")) {
         $('.btn').remove();
     }
 })
 
 // Make a function to hide and show the text
-$("#disapear").on("click",function(){
+$("#disapear").on("click", function () {
     $(".disapear").toggle(1000);
-    if($("#disapear").text() === "disapear") {
+    if ($("#disapear").text() === "disapear") {
         $("#disapear").text("show");
         $("#disapear").toggleClass("add-margin-top");
     }
     else {
         $("#disapear").text("disapear");
-        setTimeout(function() {
+        setTimeout(function () {
             $("#disapear").toggleClass("add-margin-top");
         }, 1000);
     }
